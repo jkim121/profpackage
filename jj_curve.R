71262020ae8b9f7012347e59e38f3b27b61ed1bf
jkim121 <jt.kim121@gmail.com>
2018-08-10 19:58:44 -0400

    1st commit

---
+++ jj_curve.R
@@ -0,0 +1,9 @@
+jj_curve <- function(scores){
+  curved_scores <- vector(length = length(scores))
+  
+  for(i in 1:length(scores)){
+    curved_scores[i] <- sqrt(scores[i]*100)
+  }
+  return(curved_scores)
+}
+
