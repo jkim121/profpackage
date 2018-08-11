5cef3883bfbe0f07ab2f7e7ba840e7fb97fcf3b4
jkim121 <jt.kim121@gmail.com>
2018-08-10 20:05:03 -0400

    vectorized the function

--- jj_curve.R
+++ jj_curve.R
@@ -1,9 +1,6 @@
 jj_curve <- function(scores){
-  curved_scores <- vector(length = length(scores))
-  
-  for(i in 1:length(scores)){
-    curved_scores[i] <- sqrt(scores[i]*100)
-  }
-  return(curved_scores)
+  sqrt(scores*100)
 }
+#version control
 
+jj_curve(1:100)
